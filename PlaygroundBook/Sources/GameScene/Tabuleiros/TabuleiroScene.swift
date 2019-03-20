02df75928b121eeecbd1ff12d0ce86245a72d90b
Geovane Leandro da Costa Junior <geovanedesenha@gmail.com>
2019-03-20 14:27:08 -0400

    Função para gerar catalogo de tabuleiros funcionando

--- PlaygroundBook/Sources/GameScene/Tabuleiros/TabuleiroScene.swift
+++ PlaygroundBook/Sources/GameScene/Tabuleiros/TabuleiroScene.swift
@@ -15,30 +15,29 @@ class TabuleiroScene: SKScene {
         if let labe = self.childNode(withName: "label") as? SKLabelNode{
             labe.text = " foi \(coiso2)"
         }
-        
+        creatBoard()
     }
    
     
     func creatBoard () {
         
         let A = SKSpriteNode(imageNamed: "Tabuleiro_A_char")
-        A.position = CGPoint(x: 0, y: -448)
+        A.position = CGPoint(x: 0, y: 0)
         A.size = CGSize(width: 414, height: 595)
         A.zPosition = 1
         addChild(A)
         
         let B = SKSpriteNode(imageNamed: "Tabuleiro_B_char")
-        B.position = CGPoint(x: 0, y: -448)
+        B.position = CGPoint(x: 500, y: 0)
         B.size = CGSize(width: 414, height: 595)
         B.zPosition = 1
         addChild(B)
         
         let C = SKSpriteNode(imageNamed: "Tabuleiro_C_char")
-        C.position = CGPoint(x: 0, y: -448)
+        C.position = CGPoint(x: 1000, y: 0)
         C.size = CGSize(width: 414, height: 595)
         C.zPosition = 1
         addChild(C)
-        
         
     }
 }
