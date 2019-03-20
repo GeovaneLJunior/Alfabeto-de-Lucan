44339e73bdf43a71eb1b257e7ea0c029ab13e86a
Geovane Leandro da Costa Junior <geovanedesenha@gmail.com>
2019-03-20 16:19:24 -0400

    atual

---
+++ PlaygroundBook/Sources/Model/classCard.swift
@@ -0,0 +1,17 @@
+//
+//  classCard.swift
+//  Book_Sources
+//
+//  Created by Geovane Leandro da Costa Junior on 20/03/19.
+//
+
+import Foundation
+import SpriteKit
+
+class Card: SKSpriteNode {
+    
+    override init(tipo: ) {
+        <#code#>
+    }
+    
+}
